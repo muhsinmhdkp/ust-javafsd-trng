370083154f61fb4ff1c1aeb91908330d6e7e2e0c
muhsinmhdkp <112387424+muhsinmhdkp@users.noreply.github.com>
2022-09-02 12:49:33 +0530

    Add files via upload

---
+++ JAVA/Casting.java
@@ -0,0 +1,16 @@
+package com.ust;
+
+public class Casting {
+	public static void main(String args[]) {
+		int x=5;
+		float y=5.5f;
+		long l=10;
+		double d =7.5;
+		
+		l=x; // upcasting 
+		x=(int)l; //downcasting
+		y = (float)d;
+		d=y;
+	}
+
+}
