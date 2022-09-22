3f064e8ed7a0ccf8c117e17041a5fb5953288238
muhsinmhdkp <112387424+muhsinmhdkp@users.noreply.github.com>
2022-09-22 15:02:35 +0530

    Add files via upload

--- JAVA/TESTING/Mockito1.java
+++ JAVA/TESTING/Mockito1.java
@@ -1,23 +1,28 @@
-package junit;
+package com.ust;
 
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class Mockito1 {
 
 	@Test
 	public void test() {
 		//fail("Not yet implemented");
-		System.out.println("In test method");
-		List<String> mockList = mock(List.class);
-		when(mockList.size()).thenReturn(5);
-		assertTrue(mockList.size()==7);
-		System.out.println("End of the method");
+		List<String> lst = new ArrayList<>();
+		List<String> s = spy(lst);
+		when(s.size()).thenReturn(5);
+		assertEquals(5, s.size());
+		
+		//s.add("Lekshmi");
+		//s.add("Jayanth");
+		//assertEquals("Lekshmi",s.get(0));
 	}
 
 }
