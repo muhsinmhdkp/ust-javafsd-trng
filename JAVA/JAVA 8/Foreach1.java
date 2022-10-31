b850437b8fd87e53c1623162526bcea462a733ea
muhsinmhdkp <233673@ust-global.com>
2022-10-31 17:31:38 +0530

    changes made

--- JAVA/JAVA 8/Foreach1.java
+++ JAVA/JAVA 8/Foreach1.java
@@ -3,10 +3,18 @@ package java8;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Foreach 1.
+ */
 public class Foreach1 {
+	/**
+	 * Main.
+	 *
+	 * @param args the args
+	 */
 	public static void main(String args[]) {
 		List<String> gamesList = new ArrayList<String>();
-		gamesList.add("Football");
+		gamesList.add("Fooball");
 		gamesList.add("Cricket");
 		gamesList.add("Chess");
 		gamesList.add("Hockey");
